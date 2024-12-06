8d4d8eaf389140a4c123de98d356baf26ae615a1
unknown <koti.marci@gmail.com>
2024-12-06 14:02:44 +0100

    Adding simple calculations class in py file

---
+++ simple_calcs.py
@@ -0,0 +1,16 @@
+# calculator.py
+class SimpleCalcs:
+    def addition(self, a, b):
+        return a + b
+    
+    def subtraction(self, a, b):
+        return a - b
+    
+    def multiplication(self, a, b):
+        return a * b
+    
+    def division(self, a, b):
+        if (b!=0):
+            return a/b
+        else:
+            return "Cannot divide by zero!"
